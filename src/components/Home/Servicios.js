f1c14bb1f9997381aa0574a3f2060e98f691c8dd
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2025-11-04 15:40:08 -0300

    cambios 4/11/25

--- src/components/Home/Servicios.js
+++ src/components/Home/Servicios.js
@@ -15,7 +15,6 @@ export default function Servicios() {
       <h2 className='section-title mb-5 mb-lg-0'>Servicios que brindo</h2>
       <div className='row g-3 row-cols-1 row-cols-lg-3 p-0 p-lg-5'>
         <div className='col' data-aos="fade-up" data-aos-duration="1500" data-aos-anchor-placement="top-center">
-          <Link to="/clinica-individual" >
             <div className='wrapper'>
               <div className='image'>
                 <img src="imagen44.jpg" alt="" />
@@ -23,13 +22,10 @@ export default function Servicios() {
               <div className='content'>
                 <h3 className='title'>Clínica Individual</h3>
                 <p className='desscription'>Espacio personal para superar desafíos emocionales y conectar con propósito y bienestar.</p>
-                <Link to="/clinica-individual" className='link-ser'>Mas información</Link>
               </div>
             </div>
-          </Link>
         </div>
         <div className='col' data-aos="fade-up" data-aos-duration="2000" data-aos-anchor-placement="top-center">
-          <Link to="/talleres-y-espacios-grupales">
             <div className='wrapper'>
               <div className='image'>
                 <img src="imagen6.jpg" alt="" />
@@ -37,13 +33,10 @@ export default function Servicios() {
               <div className='content'>
                 <h3 className='title'>Talleres y Espacio Grupales</h3>
                 <p className='desscription'>Dinámicas participativas para el aprendizaje, apoyo mutuo y crecimiento colectivo.</p>
-                <Link to="/talleres-y-espacios-grupales" className='link-ser'>Mas información</Link>
               </div>
             </div>
-          </Link>
         </div>
         <div className='col' data-aos="fade-up" data-aos-duration="2500" data-aos-anchor-placement="top-center">
-          <Link to="/armonización-de-campo-energético">
             <div className='wrapper'>
               <div className='image'>
                 <img src="imagen3.jpg" alt="" />
@@ -51,10 +44,8 @@ export default function Servicios() {
               <div className='content'>
                 <h3 className='title'>Armonización de campo energético a distancia con Tecnología Cuántica</h3>
                 <p className='desscription'>Armonización energética a distancia mediante tecnología cuántica para recuperar bienestar.</p>
-                <Link to="/armonización-de-campo-energético" className='link-ser'>Mas información</Link>
               </div>
             </div>
-          </Link>
         </div>
       </div>
     </div>
